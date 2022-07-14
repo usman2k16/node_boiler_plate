2fad762efe489fabd17ebe3c01b1d50f3f9f29f5
Usman Ali <code.usman@gmail.com>
2022-07-14 12:45:30 -0500

    updated directory structure

--- notFound/notFound.js
+++
@@ -1,6 +0,0 @@
-const notFound = (req,res)=>
-{
-    return res.status(404).send("Given Function is not found!")
-}
-
-module.exports = notFound;
--- routes/tasks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllTasks, addTask, getTask, updateTask,deleteTask } = require('../controllers/tasks');
-
-
-router.route('/').get(getAllTasks).post(addTask);
-router.route('/:id').get(getTask).delete(deleteTask).patch(updateTask);
-
-module.exports = router;
